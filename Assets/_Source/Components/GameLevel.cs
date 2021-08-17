7a4bf4090929bf36adc4307136a00f1308f0e948
jonesvgonc <58043335+jonesvgonc@users.noreply.github.com>
2021-08-17 10:34:22 -0300

    Fixed serialization bugs

--- Assets/_Source/Components/GameLevel.cs
+++ Assets/_Source/Components/GameLevel.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 [Serializable]
 public class GameLevel
 {
+    [SerializeField]
     public int LevelId;
+    [SerializeField]
     public List<LevelObjectPositions> LevelObjects;
 }
--- Assets/_Source/Components/LevelObjectPositions.cs
+++ Assets/_Source/Components/LevelObjectPositions.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using UnityEngine;
 
 [Serializable]
 public class LevelObjectPositions
 {
+    [SerializeField]
     private int _prefabId;
+    [SerializeField]
     private int _positionX;
+    [SerializeField]
     private int _positionY;
 
     public int PrefabId { get => _prefabId; set => _prefabId = value; }
